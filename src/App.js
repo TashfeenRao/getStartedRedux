5f6087d3c334717d39c8bdd724f878c483f50384
tashfeen rao <tashfeen@ricult.com>
2021-12-16 01:08:02 +0500

    main posts page completed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
 } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
+import PostLists from "./app/features/postLists";
+import AddNewPost from "./app/features/addNewPost";
 
 function App() {
   return (
@@ -18,9 +20,10 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <>
+                  <AddNewPost />
+                <PostLists />
+              </>
             )}
           />
           <Redirect to="/" />
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {configureStore} from '@reduxjs/toolkit'
+import postsSlice from "./features/postsSlice";
 
 export default configureStore({
-  reducer: () => ({}),
+    reducer:
+        {posts: postsSlice}
 })
